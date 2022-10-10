006318a8450492df283ef45370fcaf50688a9737
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-10-10 09:19:34 +0000

    punto 2 fatte route e rout per cercare con l'id

--- preparazione-verifica/src/app/app-routing.module.ts
+++ preparazione-verifica/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent},
+  { path: 'items', component: ItemsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
